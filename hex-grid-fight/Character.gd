69cab8d244a057f452acf6f08f38837880f65ebc
Simon Proctor <simon.proctor@zoopla.co.uk>
2020-09-15 12:33:01 +0100

    Updating Character to use Atlas

--- hex-grid-fight/Character.gd
+++ hex-grid-fight/Character.gd
@@ -1,15 +1,25 @@
 tool
 extends Node2D
 
-enum Body { RANGER = 270, SAGE = 271 }
+enum Body { RANGER, SAGE }
 export(Body) var body = Body.RANGER setget set_body
+var ranges = {
+	Body.RANGER : {
+		"x" : 0, "y": 85
+	},
+	Body.SAGE : {
+		"x" : 17, "y": 85
+	}
+}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Body.frame = body
+	print(ranges)
+#	print(Body)
+	self.set_body(body)
 
 func set_body(value):
 	body = value
-	if $Body:
-		$Body.frame = body
+	if $Body != null:
+		$Body.set_region_rect( Rect2( ranges[body]["x"], ranges[body]["y"], 16, 16 ) )
