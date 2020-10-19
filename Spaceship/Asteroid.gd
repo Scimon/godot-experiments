5d2b901137b49795b9e118006fe9a4f4879ee7cc
Simon Proctor <simon.proctor@zoopla.co.uk>
2020-10-19 13:04:06 +0100

    Updated Asteroid spawning and speed

--- Spaceship/Asteroid.gd
+++ Spaceship/Asteroid.gd
@@ -7,7 +7,7 @@ onready var shipVelocity = get_node("/root/ShipVelocity")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	velocity = Vector2(randf(), randf()).normalized() * ( 2 * ( randi() % 20  ) )
+	velocity = Vector2(randf(), randf()).normalized() * ( 4 * ( randi() % 20  ) )
 	rotation_speed = .1 - (randf() * .2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Spaceship/Asteroids.gd
+++ Spaceship/Asteroids.gd
@@ -22,25 +22,10 @@ func _on_Timer_timeout():
 		var asteroid = Asteroid.instance()
 		asteroid.global_position = target
 		$AsteroidGroup.add_child(asteroid)
-#	if $AsteroidGroup.get_child_count():
-#		$Timer.wait_time = 1 / $AsteroidGroup.get_child_count()
-#	else:
-#		$Timer.wait_time = 0.02
 
 func get_point(velocity : Vector2):
-	for vector in [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]:
-		if abs(velocity.angle_to(vector)) <= PI/4:
-			return rand_point(vector)
-	return rand_point(Vector2.UP)
+	var length = 600
+	var rotate = (randf() * PI) - (PI / 2)
+	var vector = CENTER + (velocity.normalized().rotated(rotate) * length)
+	return vector
 
-func rand_point(direction : Vector2):
-	if direction == Vector2.UP:
-		return Vector2(randf() * 1024,(randf() * 30) - 50)
-	if direction == Vector2.DOWN:
-		return Vector2(randf() * 1024,620 + (randf() * 30))
-	if direction == Vector2.LEFT:
-		return Vector2((randf() * 30) - 50,randf() * 600)
-	if direction == Vector2.RIGHT:
-		return Vector2(1044 + (randf() * 30),randf() * 600)
-	
-	
